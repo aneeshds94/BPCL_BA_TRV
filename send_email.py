592963cb37c82c2ca182d82bcd6437836c8d16f2
Aneesh <aneeshds803388@gmail.com>
2023-10-10 08:53:27 +0530

    Receiver email added

--- send_email.py
+++ send_email.py
@@ -7,7 +7,7 @@ def send_email(message):
     port = 465
     username = "shift.trv.bpcl@gmail.com"
     password = "jhilswofzwiayemt"
-    receiver = "sandeepsbabu@bharatpetroleum.in"
+    receiver = ["sandeepsbabu@bharatpetroleum.in","shift.trv@bsspl.in"]
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
